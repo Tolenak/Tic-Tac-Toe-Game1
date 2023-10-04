402313cbf6a2035541df13287afd251cc3c87f27
tyler <@tylerolenak@gmail.com>
2023-10-03 19:39:35 -0700

    adding an eventlistener to board, cell, message, and reset elements to ensure they on function after html page is fully loaded up

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// variables 
+// variables //
 
 // keeping track of the current player starting with X 
 let currentPlayer = "X"; 
@@ -16,4 +16,29 @@ let winPatterns = [
     [0,4,8], [2,4,6] // this will be the digonal combinitons 
 ]
 
+// end of variables // 
+
+
+
+// create a function that will toggle between player x and o 
+
+function switchPlayer(currentplayer) {
+    return currentPlayer === "X" ? "O" : "X"; 
+
+}
+
+// initalize the game board when the web page loads up 
+// this eventlistener will be fired as soon as the html page is fully loaded
+// attach elements inside of the eventlistener to ensure that it only starts after html pageb is fully loaded 
+ document.addEventListener("DOMContentLoaded", () => {
+    const board = document.getElementById("board");      // when page loads allows interaction with gameboard 
+    const cells = document.querySelectorAll(".cell");  // allow interaction with indiviual cells 
+    const message = document.getElementById("message");  // this is the win or draw message being displayed to players when game finishs 
+    const resetButton = document.getElementById("resetButton") // this is the button that allows the game to be reset once pushed 
+})
+
+
+// create a function for a player to win and draw between two players 
+
+// 
 
